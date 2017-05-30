64e91988ec0b6039af93da52a0ed02657c50966b
Karolis <karolis.jankauskas@gmail.com>
2017-05-30 12:28:19 +0100

    Init repo.

---
+++ SudokuSolver WPF/MainWindow.xaml.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace SudokuSolver_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BackgroundWorker bcLoad = new BackgroundWorker();
+        SudokuSolver puzzle = new SudokuSolver();
+
+        int[,] sudokuProblem;
+        int[,] answer;
+        int steps;
+        int i;
+        int j;
+        double ts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bcLoad.DoWork += new DoWorkEventHandler(bcLoad_DoWork);
+            bcLoad.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bcLoad_RunWorkerCompleted);
+        }
+
+        private void solveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            reportBox.Text = "Running...";
+
+            sudokuProblem = new int[9, 9];
+
+            i = 0;
+            j = 0;
+
+            foreach (Control txtBox in sudokuGrid.Children)
+            {
+                if (txtBox.GetType() == typeof(TextBox))
+                {
+                    try
+                    {
+                        if (((TextBox)txtBox).Text == string.Empty)
+                        {
+                            sudokuProblem[j, i] = 0;
+                        }
+                        else
+                        {
+                            sudokuProblem[j, i] = int.Parse(((TextBox)txtBox).Text);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            goto Run;
+                        }
+                        else
+                        {
+                            reportBox.Text = "Puzzle cannot be solved";
+                            return;
+                        }                     
+                    }
+
+                    i++;
+
+                    if (i > 8)
+                    {
+                        i = 0;
+                        j++;                   
+                    }
+
+                    if (j > 8 && i > 8)
+                    {
+                        goto Run;
+                    }
+                    else if (j > 8)
+                    {
+                        j = 0;
+                    }
+                }
+            }
+
+        Run:
+            try
+            {
+                solveBtn.IsEnabled = false;
+                bcLoad.RunWorkerAsync();
+            }
+            catch (Exception)
+            {
+                solveBtn.IsEnabled = true;
+                reportBox.Text = "Puzzle cannot be solved";
+            }                      
+        }
+
+        private void clearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            answer = new int[9, 9];
+            steps = 0;
+
+            foreach (Control txtBox in sudokuGrid.Children)
+            {
+                if (txtBox.GetType() == typeof(TextBox))
+                {
+                    try
+                    {
+                        ((TextBox)txtBox).Text = string.Empty;
+                    }
+                    catch (Exception)
+                    {
+                        reportBox.Text = "Something went wrong";
+                        return;
+                    }
+                }
+            }
+
+            reportBox.Text = "Enter the puzzle and click \"Solve\"";
+        }
+
+        private void bcLoad_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int[,] sudokuCopy = sudokuProblem;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (sudokuProblem[i, j] != 0)
+                    {
+                        if (!puzzle.checkIfValid(sudokuProblem, i, j, sudokuProblem[i, j])
+                            || sudokuProblem[i, j] < 0
+                            || sudokuProblem[i, j] > 9
+                            || sudokuProblem[i, j] % 1 != 0)
+                        {
+                            e.Cancel = true;
+                            return;
+                        }
+                    }
+                }
+            }
+
+            sudokuProblem = sudokuCopy;
+
+            Run();
+        }
+
+        private void bcLoad_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                solveBtn.IsEnabled = true;
+                reportBox.Text = "Puzzle cannot be solved";
+            }
+            else
+            {
+                i = j = 0;
+
+                foreach (Control txtBox in sudokuGrid.Children)
+                {
+                    if (txtBox.GetType() == typeof(TextBox))
+                    {
+                        try
+                        {
+                            ((TextBox)txtBox).Text = answer[j, i].ToString();
+                        }
+                        catch (Exception)
+                        {
+                            ((TextBox)txtBox).Text = "x";
+                        }
+
+                        i++;
+
+                        if (i > 8)
+                        {
+                            i = 0;
+                            j++;
+                        }
+                        else if (j > 8)
+                        {
+                            j = 0;
+                        }
+                    }
+                }
+
+                solveBtn.IsEnabled = true;
+
+                reportBox.Text = String.Format("Solved in {0} steps and {1} seconds", steps, ts); ;
+            }
+        }
+
+        private void Run()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            puzzle.solveSudoku(sudokuProblem, out answer, out steps);
+            stopWatch.Stop();
+
+            ts = stopWatch.ElapsedMilliseconds;
+
+            ts = ts / 1000;         
+        }
+    }
+}
---
+++ SudokuSolver WPF/SudokuSolver.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver_WPF
+{
+    public class SudokuSolver
+    {
+        public int[,] sudoku;
+        public int[] rows;
+        public int[] columns;
+        public int goBackFlag;
+        public int nextNum = 1;
+        public int c = 0;
+        public int r = 0;
+        public int iteration;
+
+        public int[,] solveSudoku(int[,] sudokuProblem, out int[,] sudokuAnswer, out int iterations)
+        {
+            sudoku = (int[,])sudokuProblem.Clone();
+
+            c = 0;
+            r = 0;
+
+            while (true)
+            {
+                if (c > 8)
+                {
+                    c = 0;
+                    r = r + 1;
+                    if (r > 8)
+                    {
+                        iterations = iteration;
+                        sudokuAnswer = sudoku;
+                        return sudokuAnswer;
+                    }
+                }
+                //else if (c < -1)
+                //{
+                //    c = 8;
+                //    r = r - 1;
+                //}
+
+                if (nextNum > 9)
+                {
+                    goBackFlag = 1;
+                    if (sudokuProblem[r, c] == 0)
+                    {
+                        sudoku[r, c] = 0;
+                    }
+                    if (c == 0)
+                    {
+                        c = 9;
+                        r = r - 1;
+                    }
+                    c = c - 1;
+                    nextNum = sudoku[r, c] + 1;
+                }
+
+                if (sudokuProblem[r, c] == 0)
+                {
+                    for (int i = nextNum; i <= 9; i++)
+                    {
+                        sudoku[r, c] = i;
+                        int row = r;
+                        int column = c;
+                        if (checkIfValid(sudoku, row, column, i))
+                        {
+                            goBackFlag = 0;
+                            sudoku[r, c] = i;
+                            c = c + 1;
+                            nextNum = 1;
+                            break;
+                        }
+                        else if (i == 9)
+                        {
+                            goBackFlag = 1;
+                            sudoku[r, c] = 0;
+                            if (c == 0)
+                            {
+                                c = 9;
+                                r = r - 1;
+                            }
+                            c = c - 1;
+                            nextNum = sudoku[r, c] + 1;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (goBackFlag == 1)
+                    {
+                        if (c == 0)
+                        {
+                            c = 9;
+                            r = r - 1;
+                        }
+                        c = c - 1;
+                        nextNum = sudoku[r, c] + 1;
+                    }
+                    else
+                    {
+                        c = c + 1;
+                    }
+                }
+                iteration = iteration + 1;
+            }
+
+            sudokuAnswer = sudoku;
+            return sudokuAnswer;
+        }
+
+        public bool checkIfValid(int[,] sudokuTemp, int row, int column, int number)
+        {
+            sudoku = (int[,])sudokuTemp.Clone();
+
+            sudoku[row, column] = 0;
+
+            if (row >= 0 && row <= 2 && column >= 0 && column <= 2)
+            {
+                rows = new int[] { 0, 1, 2 };
+                columns = new int[] { 0, 1, 2 };
+            }
+            else if (row >= 0 && row <= 2 && column >= 3 && column <= 5)
+            {
+                rows = new int[] { 0, 1, 2 };
+                columns = new int[] { 3, 4, 5 };
+            }
+            else if (row >= 0 && row <= 2 && column >= 6 && column <= 8)
+            {
+                rows = new int[] { 0, 1, 2 };
+                columns = new int[] { 6, 7, 8 };
+            }
+            else if (row >= 3 && row <= 5 && column >= 0 && column <= 2)
+            {
+                rows = new int[] { 3, 4, 5 };
+                columns = new int[] { 0, 1, 2 };
+            }
+            else if (row >= 3 && row <= 5 && column >= 3 && column <= 5)
+            {
+                rows = new int[] { 3, 4, 5 };
+                columns = new int[] { 3, 4, 5 };
+            }
+            else if (row >= 3 && row <= 5 && column >= 6 && column <= 8)
+            {
+                rows = new int[] { 3, 4, 5 };
+                columns = new int[] { 6, 7, 8 };
+            }
+            else if (row >= 6 && row <= 8 && column >= 0 && column <= 2)
+            {
+                rows = new int[] { 6, 7, 8 };
+                columns = new int[] { 0, 1, 2 };
+            }
+            else if (row >= 6 && row <= 8 && column >= 3 && column <= 5)
+            {
+                rows = new int[] { 6, 7, 8 };
+                columns = new int[] { 3, 4, 5 };
+            }
+            else if (row >= 6 && row <= 8 && column >= 6 && column <= 8)
+            {
+                rows = new int[] { 6, 7, 8 };
+                columns = new int[] { 6, 7, 8 };
+            }
+
+            int p = 0;
+            int elements = 1;
+
+            while (p <= 8)
+            {
+                if (number == sudoku[row, p] || number == sudoku[p, column])
+                {
+                    return false;
+                }
+                else if (p == 8)
+                {
+                    foreach (int r in rows)
+                    {
+                        foreach (int c in columns)
+                        {
+                            if (number == sudoku[r, c])
+                            {
+                                return false;
+                            }
+                            else if (elements == 9)
+                            {
+                                return true;
+                            }
+                            elements = elements + 1;
+                        }
+                    }
+                }
+                else
+                {
+                    p = p + 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
